1634241e7572a4efcfed2b441c3626db9802bcfc
matteo-stat <fogliata.teo@gmail.com>
2023-06-04 16:07:24 +0200

    tried to implement default bounding box generator
    from quick test seems more or less ok
    next step is to compare default boxes with ground truth
    maybe it's better to use xmin, ymin, xmax, ymax coordinates
    hope it won't be too hard :)

---
+++ ssd.py
@@ -0,0 +1,82 @@
+import tensorflow as tf
+import numpy as np
+import math
+
+def get_scale_default_bounding_boxes(k: int, m: int, scale_min: float = 0.2, scale_max: float = 0.9) -> float:
+    """
+    calculate the scale of the default boxes for a given feature map, as suggested from the original single shot multibox detector paper
+
+    Args:
+        k (int): current feature maps index (starting from 1, not 0)
+        m (int, optional): total number of feature maps . Defaults to 6.
+        scale_min (float, optional): minimum scale for default bounding boxes. Defaults to 0.2.
+        scale_max (float, optional): maximum scale for default bounding boxes. Defaults to 0.9.
+
+    Returns:
+        float: the scale for the current feature map
+    """
+    return scale_min + (scale_max - scale_min) * (k - 1) / (m - 1) if k <= m else scale_max
+
+
+def generate_default_bounding_boxes(feature_maps_shapes: tuple[tuple[int]], aspect_ratios: tuple[float] = (1.0, 2.0, 3.0, 1/2, 1/3), centers_padding_from_borders: float = 0.5) -> list[np.ndarray]:
+    """
+    generate default bounding boxes for the given feature maps shapes and aspect ratios
+    for each pixel of a feature map, a number of len(aspect_ratios) + 1 default bounding boxes are calculated
+
+    Args:
+        feature_maps_shapes (tuple[tuple[int]]): a list of shapes for the feature maps on which we want to generate default bounding boxes
+        aspect_ratios (tuple[float]): a list of aspect ratios. Defaults to (1.0, 2.0, 3.0, 1/2, 1/3).
+        centers_padding_from_borders (float): padding margin from borders for the centers grid Defaults to 0.5.
+
+    Returns:
+        list.[nd.array]: a list of multi-dimensional numpy arrays, with shape (feature_map_shape[0], feature_map_shape[1], len(aspect_ratios) + 1, 4), where the last dimension contains the following values for the bounding box: center_x, center_y, width, height
+    """
+
+    # list to store boxes for each feature map
+    feature_maps_boxes = []
+
+    # calculate boxes for each feature map
+    for feature_map_index, feature_map_shape in enumerate(feature_maps_shapes):
+        
+        # get the smallest side of the feature map shape
+        feature_map_size = min(feature_map_shape)
+
+        # get scales for current feature map and the next one
+        scale_current = get_scale_default_bounding_boxes(feature_map_index + 1, m=len(feature_maps_shapes))
+        scale_next = get_scale_default_bounding_boxes(feature_map_index + 2, m=len(feature_maps_shapes))        
+
+        # calculate width and height for each aspect ratio
+        # also calculate an additional square box with different scale, as proposed in original ssd paper
+        # the output is a list of lists like [[width, height], ..., [width, height]], same length as given aspect ratios list
+        boxes_width_height = [
+            [feature_map_size * scale_current * math.sqrt(aspect_ratio), feature_map_size * scale_current / math.sqrt(aspect_ratio)]
+            for aspect_ratio in aspect_ratios
+        ]
+        boxes_width_height.append([feature_map_size * math.sqrt(scale_current * scale_next), feature_map_size * math.sqrt(scale_current * scale_next)])
+        boxes_width_height = np.array(boxes_width_height)
+
+        # calculate centers coordinates for the boxes
+        # there is a center for each pixel in the current feature map
+        boxes_center_x = np.linspace(start=centers_padding_from_borders, stop=feature_map_shape[1] - centers_padding_from_borders, num=feature_map_shape[1])
+        boxes_center_y = np.linspace(start=centers_padding_from_borders, stop=feature_map_shape[0] - centers_padding_from_borders, num=feature_map_shape[0])
+
+        # manipulate the arrays of centers to get outputs of shape (feature_map_shape_y, feature_map_shape_x, 1)
+        boxes_center_x, boxes_center_y = np.meshgrid(boxes_center_x, boxes_center_y)
+        boxes_center_x = np.expand_dims(boxes_center_x, axis=-1)
+        boxes_center_y = np.expand_dims(boxes_center_y, axis=-1)
+        
+        # prepare final output array with shape (feature_map_shape_y, feature_map_shape_x, number_of_boxes, 4)
+        # we end up with an array containing box coordinates for each aspect ratio (+1) for each pixel in the feature map
+        boxes = np.zeros((feature_map_shape[0], feature_map_shape[1], len(boxes_width_height), 4))
+
+        # populate the output array
+        # assign to the last dimension the 4 values center_x, center_y, width, height
+        boxes[:, :, :, 0] = np.tile(boxes_center_x, (1, 1, len(boxes_width_height)))
+        boxes[:, :, :, 1] = np.tile(boxes_center_y, (1, 1, len(boxes_width_height)))
+        boxes[:, :, :, 2] = boxes_width_height[:, 0]
+        boxes[:, :, :, 3] = boxes_width_height[:, 1]
+
+        # append boxes calculated for the current feature map
+        feature_maps_boxes.append(boxes)
+
+    return feature_maps_boxes
--- super-quick-and-dirty-check.py
+++ super-quick-and-dirty-check.py
@@ -16,34 +16,142 @@ import random
 with open('data/train.json', 'r') as f:
     data = json.load(f)
 
-label_code_to_str = {
-    1: 'monorail',
-    2: 'person',
-    3: 'forklift'
-}
-
-label_code_to_color = {
-    1: 'red',
-    2: 'green',
-    3: 'blue'
-}
+# check = 'bbox_labels'
+# check = 'bbox_grid_default'
+check = 'bbox_default'
 
 for path_image, path_mask, labels in random.sample(data, 10):
     image = tf.io.read_file(path_image)
     image = tf.image.decode_png(image, channels=3)
     image = tf.cast(image, dtype=tf.float32) / 255.0
 
-    # display the image
-    plt.imshow(image, vmin=0, vmax=1)
-    plt.axis('off')
+    if check == 'bbox_labels':
+        # display the image
+        plt.imshow(image, vmin=0, vmax=1)
+        plt.axis('off')
 
-    # get the current plot object
-    ax = plt.gca()
+        # get the current plot object
+        ax = plt.gca()
 
-    # plot bounding boxes
-    for label, xmin, ymin, xmax, ymax, cx, cy, w, h in labels:
-        rect = patches.Rectangle((xmin, ymin), w, h, linewidth=1, edgecolor=label_code_to_color[label], facecolor='none')
-        ax.add_patch(rect)
-        plt.text(xmin, ymin, label_code_to_str[label], fontsize=8, color=label_code_to_color[label], verticalalignment='top')
+        # labels conversions
+        label_code_to_str = {
+            1: 'monorail',
+            2: 'person',
+            3: 'forklift'
+        }
+        label_code_to_color = {
+            1: 'red',
+            2: 'green',
+            3: 'blue'
+        }
 
-    plt.show()
+        # plot bounding boxes
+        for label, xmin, ymin, xmax, ymax, cx, cy, w, h in labels:
+            rect = patches.Rectangle((xmin, ymin), w, h, linewidth=1, edgecolor=label_code_to_color[label], facecolor='none')
+            ax.add_patch(rect)
+            plt.text(xmin, ymin, label_code_to_str[label], fontsize=8, color=label_code_to_color[label], verticalalignment='top')
+    
+    elif check == 'bbox_grid_default':
+        # create subplots and set figure size
+        fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3)
+        fig.set_size_inches(30, 10)
+
+        # image subplot
+        ax1.imshow(image, vmin=0, vmax=1)
+        ax1.set_axis_off()
+        ax1.set_title('grid feat map 1')
+        
+        # mask real subplot (drop background channel)        
+        ax2.imshow(image, vmin=0, vmax=1)
+        ax2.set_axis_off()
+        ax2.set_title('grid feat map 2')
+
+        # mask predicted subplot (drop background channel)
+        ax3.imshow(image, vmin=0, vmax=1)
+        ax3.set_axis_off()
+        ax3.set_title('grid feat map 3')
+
+        # example feature maps shapes
+        feature_maps_shapes = (
+            (48, 64),
+            (24, 32),
+            (12, 16)
+        )
+
+        import ssd
+
+        # generate bounding boxes
+        feature_maps_boxes = ssd.generate_default_bounding_boxes(feature_maps_shapes=feature_maps_shapes)
+
+        # convert to image coordinates and plot
+        for boxes, feature_map_shape, color, ax in zip(feature_maps_boxes, feature_maps_shapes, ('red', 'blue', 'green'), (ax1, ax2, ax3)):
+            points_x = []
+            points_y = []
+
+            for cx, cy, w, h in boxes.reshape((-1, 4)):
+                cx = cx / feature_map_shape[1] * image.shape[1]
+                cy = cy / feature_map_shape[0] * image.shape[0]
+                w = w / feature_map_shape[1] * image.shape[1]
+                h = h / feature_map_shape[0] * image.shape[0]
+
+                points_x.append(cx)
+                points_y.append(cy)
+
+            ax.scatter(x=points_x, y=points_y, color=color, marker='o', s=1)
+
+            # break
+
+
+        plt.show()
+
+    elif check == 'bbox_default':
+        # create subplots and set figure size
+        fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3)
+        fig.set_size_inches(20, 8)
+
+        # image subplot
+        ax1.imshow(image, vmin=0, vmax=1)
+        ax1.set_axis_off()
+        ax1.set_title('bbox feat map 1')
+        
+        # mask real subplot (drop background channel)        
+        ax2.imshow(image, vmin=0, vmax=1)
+        ax2.set_axis_off()
+        ax2.set_title('bbox feat map 2')
+
+        # mask predicted subplot (drop background channel)
+        ax3.imshow(image, vmin=0, vmax=1)
+        ax3.set_axis_off()
+        ax3.set_title('bbox feat map 3')
+
+        # example feature maps shapes
+        feature_maps_shapes = (
+            (48, 64),
+            (24, 32),
+            (12, 16)
+        )
+
+        import ssd
+
+        # generate bounding boxes
+        feature_maps_boxes = ssd.generate_default_bounding_boxes(feature_maps_shapes=feature_maps_shapes)
+
+        # convert to image coordinates and plot
+        for boxes, feature_map_shape, color, ax in zip(feature_maps_boxes, feature_maps_shapes, ('red', 'blue', 'green'), (ax1, ax2, ax3)):
+            points_x = []
+            points_y = []
+
+            boxes_sample = boxes[int(feature_map_shape[0] / 2), int(feature_map_shape[1] / 2)]
+            boxes_sample = boxes_sample.reshape((-1, 4))
+            
+            for cx, cy, w, h in boxes_sample:
+                cx = cx / feature_map_shape[1] * image.shape[1]
+                cy = cy / feature_map_shape[0] * image.shape[0]
+                w = w / feature_map_shape[1] * image.shape[1]
+                h = h / feature_map_shape[0] * image.shape[0]
+
+                ax.add_patch(patches.Rectangle((cx - w/2, cy - h/2), w, h, linewidth=1, edgecolor=color, facecolor='none'))
+            # break
+
+
+        plt.show()
