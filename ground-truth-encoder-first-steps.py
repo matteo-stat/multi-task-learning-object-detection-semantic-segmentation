ab67249564d11258d5aa5269fb24af4ff5a32312
matteo-stat <fogliata.teo@gmail.com>
2023-07-02 20:07:13 +0200

    example about how to encode data completed :smiley_cat:
    now must try on real input data and figure out the data generator

--- ground-truth-encoder-first-steps.py
+++ ground-truth-encoder-first-steps.py
@@ -1,14 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+import ssd
 
-gt_boxes = [
+labels_ground_truth = np.array([3, 2, 2])
+
+boxes_ground_truth = np.array([
     [3, 4, 11, 13],
     [41, 5, 46, 10],
     [20, 13, 29, 28],
-]
+])
 
-def_boxes = [
+boxes_default = np.array([
     [3, 26, 11, 32],
     [5, 25, 9, 33],
     [6, 28, 8, 30],
@@ -28,14 +31,11 @@ def_boxes = [
     [28, 8, 36, 14],
     [30, 7, 34, 15],
     [31, 10, 33, 13],
-]
+])
 
 # args
-iou_threshold = 0
-
-# default bounding boxes and ground truth bounding boxes
-boxes_default = np.array(def_boxes)
-boxes_ground_truth = np.array(gt_boxes)
+iou_threshold = 0.5
+num_classes = 4
 
 # corners coordinates for default an ground truth bounding boxes
 xmin_boxes_default, ymin_boxes_default, xmax_boxes_default, ymax_boxes_default = np.split(boxes_default, 4, axis=-1)
@@ -47,36 +47,47 @@ area_boxes_default = (ymax_boxes_default - ymin_boxes_default + 1) * (xmax_boxes
 
 # coordinates of intersections between each default bounding box and all ground truth bounding boxes
 # it select the maximum for xmin and ymin coordinates, the minimum for xmax and ymax coordinates
-xmin_boxes_intersections = np.maximum(xmin_boxes_default, xmin_boxes_ground_truth.T)
-ymin_boxes_intersections = np.maximum(ymin_boxes_default, ymin_boxes_ground_truth.T)
-xmax_boxes_intersections = np.minimum(xmax_boxes_default, xmax_boxes_ground_truth.T)
-ymax_boxes_intersections = np.minimum(ymax_boxes_default, ymax_boxes_ground_truth.T)
+xmin_boxes_intersection = np.maximum(xmin_boxes_default, xmin_boxes_ground_truth.T)
+ymin_boxes_intersection = np.maximum(ymin_boxes_default, ymin_boxes_ground_truth.T)
+xmax_boxes_intersection = np.minimum(xmax_boxes_default, xmax_boxes_ground_truth.T)
+ymax_boxes_intersection = np.minimum(ymax_boxes_default, ymax_boxes_ground_truth.T)
 
 # area of intersection between each default bounding box and all ground truth bounding boxes
-area_intersection = np.maximum(0, xmax_boxes_intersections - xmin_boxes_intersections + 1) * np.maximum(0, ymax_boxes_intersections - ymin_boxes_intersections + 1)
-
-# area of union between each default bounding box and all ground truth bounding boxes
-area_union = area_boxes_default + area_boxes_ground_truth.T - area_intersection
+area_boxes_intersection = np.maximum(0, xmax_boxes_intersection - xmin_boxes_intersection + 1) * np.maximum(0, ymax_boxes_intersection - ymin_boxes_intersection + 1)
 
 # calculate intersection over union between each default bounding box and all ground truth bounding boxes
 # note that this it's a matrix with shape (num default bounding boxes, num ground truth bounding boxes)
-iou = area_intersection / area_union
+iou = area_boxes_intersection / (area_boxes_default + area_boxes_ground_truth.T - area_boxes_intersection)
 
 # find best match between each ground truth box and all default bounding boxes
 # note that output shape will be (num ground truth boxes with iou > 0 with at least one default box, 2)
 # the last axis contains indexes for default boxes, ground truth boxes
-best_matches_ground_truth_default_boxes = np.column_stack([iou.argmax(axis=0), np.arange(len(boxes_ground_truth))])[iou.max(axis=0) > 0]
+indexes_match_ground_truth = np.column_stack([iou.argmax(axis=0), np.arange(len(boxes_ground_truth))])[iou.max(axis=0) > 0]
 
 # find best match between each default box and all ground truth bounding boxes
 # note that output shape will be (num default truth boxes with iou > threshold with at least one ground truth box, 2)
 # the last axis contains indexes for default boxes, ground truth boxes
-best_matches_default_ground_truth_boxes = np.column_stack([np.arange(len(boxes_default)), iou.argmax(axis=1)])[iou.max(axis=1) > iou_threshold]
+indexes_match_default = np.column_stack([np.arange(len(boxes_default)), iou.argmax(axis=1)])[iou.max(axis=1) > iou_threshold]
 
 # put best matches together, removing possible pair duplicates
-best_matches = np.unique(np.vstack([best_matches_ground_truth_default_boxes, best_matches_default_ground_truth_boxes]), axis=0)
+indexes_match = np.unique(np.vstack([indexes_match_ground_truth, indexes_match_default]), axis=0)
+
+# get the class labels for each best match and one hot encode them (0 it's reserved for background class)
+labels_match = labels_ground_truth[indexes_match[:, 1]]
+labels_match = np.eye(num_classes)[labels_match]
+
+# convert boxes coordinates from corners to centroids
+centroids_default = ssd.bounding_boxes_corners_to_centroids(boxes=boxes_default[indexes_match[:, 0]])
+centroids_ground_truth = ssd.bounding_boxes_corners_to_centroids(boxes=boxes_ground_truth[indexes_match[:, 1]])
+
+# encode data as required (one hot encoding for classes, offsets for centroids coordinates)
+data_encoded = np.zeros(shape=(len(boxes_default), num_classes + 4))
+data_encoded[indexes_match[:,0], :num_classes] = labels_match
+data_encoded[indexes_match[:,0], -4:-2] = (centroids_ground_truth[:, [0, 1]] - centroids_default[:, [0, 1]]) / centroids_default[:, [2, 3]]
+data_encoded[indexes_match[:,0], -2:] = np.log(centroids_ground_truth[:, [2, 3]] / centroids_default[:, [2, 3]])
 
 # keep only best matches
-def_boxes = boxes_default[best_matches[:, 0]].tolist()
+boxes_to_plot = boxes_default[indexes_match[:,0]].tolist()
 
 # create a figure and axis
 fig, ax = plt.subplots()
@@ -86,13 +97,13 @@ ax.set_xlim([0, 47])
 ax.set_ylim([0, 37])
 
 # plot the ground truth boxes
-for box in gt_boxes:
+for box in boxes_ground_truth:
     xmin, ymin, xmax, ymax = box
     rect = patches.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin, linewidth=1, edgecolor='r', facecolor='none')
     ax.add_patch(rect)
 
 # plot the default boxes
-for box in def_boxes:
+for box in boxes_to_plot:
     xmin, ymin, xmax, ymax = box
     rect = patches.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin, linewidth=1, edgecolor='b', facecolor='none')
     ax.add_patch(rect)
