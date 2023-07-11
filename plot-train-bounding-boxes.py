f104b28e949153532702ae7a98663858a7be707b
Fogliata Matteo <fom5cer@bosch.com>
2023-07-11 22:01:39 +0200

    Update plot-train-bounding-boxes.py

--- plot-train-bounding-boxes.py
+++ plot-train-bounding-boxes.py
@@ -1,6 +1,7 @@
 import plot
 import random
 import json
+import csv
 import numpy as np
 from matplotlib import pyplot as plt, patches, get_backend
 from PIL import Image
@@ -22,7 +23,11 @@ with open('data/train.json', 'r') as f:
     data = json.load(f)
 
 # sample training data
-for path_image, path_mask, labels, boxes in random.sample(data, k=5):
+for path_image, path_mask, path_labels_boxes in random.sample(data, k=5):
+
+    # read labels boxes
+    with open(path_labels_boxes, 'r') as f:
+        labels_boxes = list(csv.reader(f))
     
     # read image
     image = Image.open(path_image)
@@ -41,7 +46,12 @@ for path_image, path_mask, labels, boxes in random.sample(data, k=5):
     ax = plt.gca()
 
     # plot bounding boxes
-    for label, (xmin, ymin, xmax, ymax) in zip(labels, boxes):
+    for label, xmin, ymin, xmax, ymax in labels_boxes:
+        label = int(label)
+        xmin = float(xmin)
+        ymin = float(ymin)
+        xmax = float(xmax)
+        ymax = float(ymax)
         rect = patches.Rectangle((xmin, ymin), xmax - xmin + 1, ymax - ymin + 1, linewidth=1, edgecolor=label_code_to_color[label], facecolor='none')
         ax.add_patch(rect)
         plt.text(xmin, ymin, label_code_to_str[label], fontsize=8, color=label_code_to_color[label], verticalalignment='top')
