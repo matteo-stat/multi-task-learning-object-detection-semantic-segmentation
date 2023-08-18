9caf4c36289693b9b83c6d64a2f60bccc5e38420
matteo-stat <fogliata.teo@gmail.com>
2023-08-18 22:50:42 +0200

    added segmentation metric

---
+++ ssdseglib/metrics.py
@@ -0,0 +1,48 @@
+from typing import List, Callable
+import tensorflow as tf
+
+def jaccard_iou(classes_weights: List[float]) -> Callable[[tf.Tensor, tf.Tensor], tf.Tensor]:
+    """
+    jaccard iou metric, for one-hot encoded semantic segmentation masks with shape (batch, height, width, number of classes)\n
+    you must pass some weights for you classes, and they must sum up to 1 (otherwise the calculation of the loss won't be right)\n
+    predictions must be passed as probabilities
+
+    Args:
+        classes_weights (List[float]): weights for your classes, they must sum up to 1 (otherwise the calculation of the loss won't be right)
+
+    Returns:
+        Callable[[tf.Tensor, tf.Tensor], tf.Tensor]: the function for calculating the weighted jaccard iou metric
+    """
+
+    classes_weights = tf.constant(classes_weights, dtype=tf.float32, shape=(1, len(classes_weights)))
+
+    def metric(y_true: tf.Tensor, y_pred: tf.Tensor) -> tf.Tensor:
+        """
+        jaccard iou metric, for one-hot encoded semantic segmentation masks with shape (batch, height, width, number of classes)\n
+        it return a single scalar loss value per batch item, which is a weighted average of the classes losses
+
+        Args:
+            y_true (tf.Tensor): ground truth
+            y_pred (tf.Tensor): predictions, expressed as probabilities
+
+        Returns:
+            tf.Tensor: a tensor with a single scalar loss value per batch item, output shape it's (batch,)
+        """
+        
+        # intersection area, along height and width dimensions, output shape it's (batch, number of classes)
+        intersection = tf.math.reduce_sum(y_true * y_pred, axis=(1, 2))
+
+        # total area, along height and width dimensions, output shape it's (batch, number of classes)
+        total = tf.math.reduce_sum(y_true + y_pred, axis=(1, 2))
+
+        # jaccard iou metric, with laplace smoothing for managing missing class values
+        # union can be calculated easily as difference between total and intersection
+        metric_value = (intersection + 1.0) / (total - intersection + 1.0)
+
+        # weighted average along classes dimension, output shape it's (batch,)
+        metric_value = metric_value * classes_weights
+        metric_value = tf.reduce_sum(metric_value, axis=-1)
+
+        return metric_value
+    
+    return metric
---
+++ test-metric-function-segmentation.py
@@ -0,0 +1,29 @@
+import numpy as np
+import tensorflow as tf
+import ssdseglib
+
+def generate_ground_truth_mask(batch_size, height, width, num_classes):
+    mask_labels = np.random.randint(0, num_classes, size=(batch_size, height, width))
+    one_hot_masks = np.eye(num_classes)[mask_labels]
+    one_hot_masks = one_hot_masks.astype(np.float32)
+    
+    return tf.convert_to_tensor(one_hot_masks)
+
+def generate_predicted_mask(batch_size, height, width, num_classes):
+    predicted_mask = np.random.random(size=(batch_size, height, width, num_classes))
+    predicted_mask /= np.sum(predicted_mask, axis=-1, keepdims=True)
+    predicted_mask = predicted_mask.astype(np.float32)
+
+    return tf.convert_to_tensor(predicted_mask)
+
+batch_size = 8
+height = 2
+width = 3
+num_classes = 4
+
+y_true = generate_ground_truth_mask(batch_size, height, width, num_classes)
+y_pred = generate_predicted_mask(batch_size, height, width, num_classes)
+
+# --------------------------------------------------------------------------------------------------
+jaccard_iou_metric = ssdseglib.metrics.jaccard_iou(classes_weights=[0., 0.4, 0.3, 0.3])
+print(jaccard_iou_metric(y_true, y_pred))
