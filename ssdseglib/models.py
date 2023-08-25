fc57986a9c2f83c0f550b50d29f8ad56be030d1d
matteo-stat <fogliata.teo@gmail.com>
2023-08-25 22:33:11 +0200

    fix errors

--- ssdseglib/models.py
+++ ssdseglib/models.py
@@ -1,4 +1,5 @@
 from typing import Tuple, Union, List
+from numpy import ndarray
 import tensorflow as tf
 import ssdseglib
 
@@ -8,45 +9,36 @@ class MobileNetV2SsdSegBuilder():
             input_image_shape: Tuple[int, int, int],
             number_of_boxes_per_point: Union[int, List[int]],
             number_of_classes: int,
-            center_x_boxes_default: tf.Tensor,
-            center_y_boxes_default: tf.Tensor,
-            width_boxes_default: tf.Tensor,
-            height_boxes_default: tf.Tensor,
-            standard_deviation_center_x_offsets: float,
-            standard_deviation_center_y_offsets: float,
-            standard_deviation_width_offsets: float,
-            standard_deviation_height_offsets: float,                        
+            center_x_boxes_default: ndarray[float],
+            center_y_boxes_default: ndarray[float],
+            width_boxes_default: ndarray[float],
+            height_boxes_default: ndarray[float],
+            standard_deviations_centroids_offsets: tuple[float],
         ) -> None:
         """
-        _summary_
+        initialize the mobilenet-v2 builder\n
+        this class can build a mobilenet-v2 backbone with a semantic segmentation head and an object detection head
 
         Args:
-            input_image_shape (Tuple[int, int, int]): _description_
-            number_of_boxes_per_point (Union[int, List[int]]): _description_
-            number_of_classes (int): _description_
-            center_x_boxes_default (tf.Tensor): center_x coordinates for default bounding boxes, expected shape it's (total boxes,)
-            center_y_boxes_default (tf.Tensor): center_y coordinates for default bounding boxes, expected shape it's (total boxes,)
-            width_boxes_default (tf.Tensor): width coordinates for default bounding boxes, expected shape it's (total boxes,)
-            height_boxes_default (tf.Tensor): height coordinates for default bounding boxes, expected shape it's (total boxes,)
-            standard_deviation_center_x_offsets (float): standard deviation for center_x offsets
-            standard_deviation_center_y_offsets (float): standard deviation for center_y offsets
-            standard_deviation_width_offsets (float): standard deviation for width offsets
-            standard_deviation_height_offsets (float): standard deviation for height offsets
+            input_image_shape (Tuple[int, int, int]): the input image shape as (height, width, channels)
+            number_of_boxes_per_point (Union[int, List[int]]): number of default bounding boxes for each point in default grids
+            number_of_classes (int): number of classes for the object detection head
+            
+            center_x_boxes_default (ndarray[float]): array of coordinates for center x (centroids coordinates)
+            center_y_boxes_default (ndarray[float]): array of coordinates for center y (centroids coordinates)
+            width_boxes_default (ndarray[float]): array of coordinates for width (centroids coordinates)
+            height_boxes_default (ndarray[float]): array of coordinates for heigh (centroids coordinates)
+            standard_deviations_centroids_offsets (tuple[float]): standard deviations for offsets between ground truth and default bounding boxes, expected as (standard_deviation_center_x_offsets, standard_deviation_center_y_offsets, standard_deviation_width_offsets, standard_deviation_height_offsets).
         """
 
         self.input_image_shape = input_image_shape
         self.number_of_boxes_per_point = (number_of_boxes_per_point,) * 4 if isinstance(number_of_boxes_per_point, int) else  number_of_boxes_per_point
         self.number_of_classes = number_of_classes
-
         self._center_x_boxes_default = center_x_boxes_default
         self._center_y_boxes_default = center_y_boxes_default
         self._width_boxes_default = width_boxes_default
         self._height_boxes_default = height_boxes_default
-
-        self._standard_deviation_center_x_offsets = standard_deviation_center_x_offsets
-        self._standard_deviation_center_y_offsets = standard_deviation_center_y_offsets
-        self._standard_deviation_width_offsets = standard_deviation_width_offsets
-        self._standard_deviation_height_offsets = standard_deviation_height_offsets
+        self._standard_deviation_center_x_offsets, self._standard_deviation_center_y_offsets, self._standard_deviation_width_offsets, self._standard_deviation_height_offsets = standard_deviations_centroids_offsets
         
         # this is a counter used to give proper names to the network components
         self._counter_blocks = 0
@@ -222,18 +214,18 @@ class MobileNetV2SsdSegBuilder():
         # IMPORTANT -> maybe you don't need these additional blocks.. i'm using them to further reduce the size of my input images with VGA resolution of 640x480
         self._counter_blocks += 1
         name_prefix = f'backbone-block{self._counter_blocks}-'
-        layer = tf.keras.layers.DepthwiseConv2D(kernel_size=3, strides=2, padding='same', depth_multiplier=1, use_bias=False, name=f'{name_prefix}depthconv')(layer)        
-        layer = tf.keras.layers.BatchNormalization(name=f'{name_prefix}depthconv-batchnorm')(layer)
-        layer = tf.keras.layers.ReLU(max_value=6.0, name=f'{name_prefix}depthconv-relu6')(layer)
+        layer = tf.keras.layers.DepthwiseConv2D(kernel_size=3, strides=2, padding='same', depth_multiplier=1, use_bias=False, name=f'{name_prefix}depthwiseconv')(layer)        
+        layer = tf.keras.layers.BatchNormalization(name=f'{name_prefix}depthwiseconv-batchnorm')(layer)
+        layer = tf.keras.layers.ReLU(max_value=6.0, name=f'{name_prefix}depthwiseconv-relu6')(layer)
         layer = tf.keras.layers.Conv2D(filters=320, kernel_size=1, padding='same', use_bias=False, name=f'{name_prefix}pointconv')(layer)
         layer = tf.keras.layers.BatchNormalization(name=f'{name_prefix}pointconv-batchnorm')(layer)
         layer = tf.keras.layers.ReLU(max_value=6.0, name=f'{name_prefix}pointconv-relu6')(layer)
 
         self._counter_blocks += 1
         name_prefix = f'backbone-block{self._counter_blocks}-'
-        layer = tf.keras.layers.DepthwiseConv2D(kernel_size=3, strides=2, padding='same', depth_multiplier=1, use_bias=False, name=f'{name_prefix}depthconv')(layer)        
-        layer = tf.keras.layers.BatchNormalization(name=f'{name_prefix}depthconv-batchnorm')(layer)
-        layer = tf.keras.layers.ReLU(max_value=6.0, name=f'{name_prefix}depthconv-relu6')(layer)
+        layer = tf.keras.layers.DepthwiseConv2D(kernel_size=3, strides=2, padding='same', depth_multiplier=1, use_bias=False, name=f'{name_prefix}depthwiseconv')(layer)        
+        layer = tf.keras.layers.BatchNormalization(name=f'{name_prefix}depthwiseconv-batchnorm')(layer)
+        layer = tf.keras.layers.ReLU(max_value=6.0, name=f'{name_prefix}depthwiseconv-relu6')(layer)
         layer = tf.keras.layers.Conv2D(filters=320, kernel_size=1, padding='same', use_bias=False, name=f'{name_prefix}pointconv')(layer)
         layer = tf.keras.layers.BatchNormalization(name=f'{name_prefix}pointconv-batchnorm')(layer)
         layer_output = tf.keras.layers.ReLU(max_value=6.0, name=f'{name_prefix}pointconv-relu6')(layer)
@@ -262,7 +254,7 @@ class MobileNetV2SsdSegBuilder():
         Returns:
             tf.keras.layers.Layer: output from a ssdlite block
         """
-        layer = tf.keras.layers.DepthwiseConv2D(depth_multiplier=1, kernel_size=3, padding='same', use_bias=False, name=f'{name_prefix}-depthconv')(layer)
+        layer = tf.keras.layers.DepthwiseConv2D(depth_multiplier=1, kernel_size=3, padding='same', use_bias=False, name=f'{name_prefix}-depthwiseconv')(layer)
         layer = tf.keras.layers.BatchNormalization(name=f'{name_prefix}-batchnorm')(layer)
         layer = tf.keras.layers.ReLU(max_value=6.0, name=f'{name_prefix}-relu6')(layer)
         layer = tf.keras.layers.Conv2D(filters=filters, kernel_size=1, padding='same', use_bias=True, name=f'{name_prefix}-pointconv')(layer)
@@ -298,25 +290,25 @@ class MobileNetV2SsdSegBuilder():
         layer_pointwise = tf.keras.layers.ReLU(name=f'{name_prefix}conv-relu')(layer_pointwise)
 
         # aspp block - 1st atrous separable convolution branch
-        layer_atrous_1 = tf.keras.layers.DepthwiseConv2D(depth_multiplier=1, kernel_size=3, padding='same', dilation_rate=dilation_rates[0], use_bias=False, name=f'{name_prefix}atrous1-depthconv')(layer)
-        layer_atrous_1 = tf.keras.layers.BatchNormalization(name=f'{name_prefix}atrous1-depthconv-batchnorm')(layer_atrous_1)
-        layer_atrous_1 = tf.keras.layers.ReLU(name=f'{name_prefix}atrous1-depthconv-relu')(layer_atrous_1)
+        layer_atrous_1 = tf.keras.layers.DepthwiseConv2D(depth_multiplier=1, kernel_size=3, padding='same', dilation_rate=dilation_rates[0], use_bias=False, name=f'{name_prefix}atrous1-depthwiseconv')(layer)
+        layer_atrous_1 = tf.keras.layers.BatchNormalization(name=f'{name_prefix}atrous1-depthwiseconv-batchnorm')(layer_atrous_1)
+        layer_atrous_1 = tf.keras.layers.ReLU(name=f'{name_prefix}atrous1-depthwiseconv-relu')(layer_atrous_1)
         layer_atrous_1 = tf.keras.layers.Conv2D(filters=filters, kernel_size=1, padding='same', use_bias=False, name=f'{name_prefix}atrous1-conv')(layer_atrous_1)
         layer_atrous_1 = tf.keras.layers.BatchNormalization(name=f'{name_prefix}atrous1-conv-batchnorm')(layer_atrous_1)
         layer_atrous_1 = tf.keras.layers.ReLU(name=f'{name_prefix}atrous1-conv-relu')(layer_atrous_1)
 
         # aspp block - 2nd atrous separable convolution branch
-        layer_atrous_2 = tf.keras.layers.DepthwiseConv2D(depth_multiplier=1, kernel_size=3, padding='same', dilation_rate=dilation_rates[1], use_bias=False, name=f'{name_prefix}atrous2-depthconv')(layer)
-        layer_atrous_2 = tf.keras.layers.BatchNormalization(name=f'{name_prefix}atrous2-depthconv-batchnorm')(layer_atrous_2)
-        layer_atrous_2 = tf.keras.layers.ReLU(name=f'{name_prefix}atrous2-depthconv-relu')(layer_atrous_2)
+        layer_atrous_2 = tf.keras.layers.DepthwiseConv2D(depth_multiplier=1, kernel_size=3, padding='same', dilation_rate=dilation_rates[1], use_bias=False, name=f'{name_prefix}atrous2-depthwiseconv')(layer)
+        layer_atrous_2 = tf.keras.layers.BatchNormalization(name=f'{name_prefix}atrous2-depthwiseconv-batchnorm')(layer_atrous_2)
+        layer_atrous_2 = tf.keras.layers.ReLU(name=f'{name_prefix}atrous2-depthwiseconv-relu')(layer_atrous_2)
         layer_atrous_2 = tf.keras.layers.Conv2D(filters=filters, kernel_size=1, padding='same', use_bias=False, name=f'{name_prefix}atrous2-conv')(layer_atrous_2)
         layer_atrous_2 = tf.keras.layers.BatchNormalization(name=f'{name_prefix}atrous2-conv-batchnorm')(layer_atrous_2)
         layer_atrous_2 = tf.keras.layers.ReLU(name=f'{name_prefix}atrous2-conv-relu')(layer_atrous_2)
 
         # aspp block - 3rd atrous separable convolution branch
-        layer_atrous_3 = tf.keras.layers.DepthwiseConv2D(depth_multiplier=1, kernel_size=3, padding='same', dilation_rate=dilation_rates[2], use_bias=False, name=f'{name_prefix}atrous3-depthconv')(layer)
-        layer_atrous_3 = tf.keras.layers.BatchNormalization(name=f'{name_prefix}atrous3-depthconv-batchnorm')(layer_atrous_3)
-        layer_atrous_3 = tf.keras.layers.ReLU(name=f'{name_prefix}atrous3-depthconv-relu')(layer_atrous_3)
+        layer_atrous_3 = tf.keras.layers.DepthwiseConv2D(depth_multiplier=1, kernel_size=3, padding='same', dilation_rate=dilation_rates[2], use_bias=False, name=f'{name_prefix}atrous3-depthwiseconv')(layer)
+        layer_atrous_3 = tf.keras.layers.BatchNormalization(name=f'{name_prefix}atrous3-depthwiseconv-batchnorm')(layer_atrous_3)
+        layer_atrous_3 = tf.keras.layers.ReLU(name=f'{name_prefix}atrous3-depthwiseconv-relu')(layer_atrous_3)
         layer_atrous_3 = tf.keras.layers.Conv2D(filters=filters, kernel_size=1, padding='same', use_bias=False, name=f'{name_prefix}atrous3-conv')(layer_atrous_3)
         layer_atrous_3 = tf.keras.layers.BatchNormalization(name=f'{name_prefix}atrous3-conv-batchnorm')(layer_atrous_3)
         layer_atrous_3 = tf.keras.layers.ReLU(name=f'{name_prefix}atrous3-conv-relu')(layer_atrous_3)
@@ -392,17 +384,17 @@ class MobileNetV2SsdSegBuilder():
         # note for me -> if there are enough resources switch to standard convolution
 
         # first depthwise separable convolution
-        layer = tf.keras.layers.DepthwiseConv2D(depth_multiplier=1, kernel_size=3, padding='same', use_bias=False, name=f'{name_prefix}depthconv1')(layer_concat)
-        layer = tf.keras.layers.BatchNormalization(name=f'{name_prefix}depthconv1-batchnorm')(layer)
-        layer = tf.keras.layers.ReLU(name=f'{name_prefix}depthconv1-relu')(layer)
+        layer = tf.keras.layers.DepthwiseConv2D(depth_multiplier=1, kernel_size=3, padding='same', use_bias=False, name=f'{name_prefix}depthwiseconv1')(layer_concat)
+        layer = tf.keras.layers.BatchNormalization(name=f'{name_prefix}depthwiseconv1-batchnorm')(layer)
+        layer = tf.keras.layers.ReLU(name=f'{name_prefix}depthwiseconv1-relu')(layer)
         layer = tf.keras.layers.Conv2D(filters=filters_decoder, kernel_size=1, padding='same', use_bias=False, name=f'{name_prefix}conv1')(layer)
         layer = tf.keras.layers.BatchNormalization(name=f'{name_prefix}conv1-batchnorm')(layer)
         layer = tf.keras.layers.ReLU(name=f'{name_prefix}conv1-relu')(layer)
 
         # second depthwise separable convolution
-        layer = tf.keras.layers.DepthwiseConv2D(depth_multiplier=1, kernel_size=3, padding='same', use_bias=False, name=f'{name_prefix}depthconv2')(layer_concat)
-        layer = tf.keras.layers.BatchNormalization(name=f'{name_prefix}depthconv2-batchnorm')(layer)
-        layer = tf.keras.layers.ReLU(name=f'{name_prefix}depthconv2-relu')(layer)
+        layer = tf.keras.layers.DepthwiseConv2D(depth_multiplier=1, kernel_size=3, padding='same', use_bias=False, name=f'{name_prefix}depthwiseconv2')(layer_concat)
+        layer = tf.keras.layers.BatchNormalization(name=f'{name_prefix}depthwiseconv2-batchnorm')(layer)
+        layer = tf.keras.layers.ReLU(name=f'{name_prefix}depthwiseconv2-relu')(layer)
         layer = tf.keras.layers.Conv2D(filters=filters_decoder, kernel_size=1, padding='same', use_bias=False, name=f'{name_prefix}conv2')(layer)
         layer = tf.keras.layers.BatchNormalization(name=f'{name_prefix}conv2-batchnorm')(layer)
         layer = tf.keras.layers.ReLU(name=f'{name_prefix}conv2-relu')(layer)
@@ -519,8 +511,37 @@ class MobileNetV2SsdSegBuilder():
         
         return model
 
-    def get_model_for_inference(self, model_trained: tf.keras.Model):
+    def get_model_for_inference(
+            self,
+            model_trained: tf.keras.Model,
+            max_number_of_boxes_per_class: int,
+            max_number_of_boxes_per_sample: int,
+            boxes_iou_threshold: float,
+            labels_probability_threshold: float,            
+        ) -> tf.keras.Model:
         """
+        transform a trained model to an inference one\b
+        it adds a layer for decoding boxes coordinates predictions and apply non-maximum-suppression to the object detection outputs, to keep only relevant boxes\n
+        the learned weights are kept from the trained model
+
+        Args:
+            model_trained (tf.keras.Model): a trained model
+            max_number_of_boxes_per_class (int): maximum number of boxes to keep per class
+            max_number_of_boxes_per_sample (int): maximum number of boxes per sample
+            boxes_iou_threshold (float): threshold for deciding whether boxes overlap too much with respect to iou
+            labels_probability_threshold (float): threshold for deciding when to remove boxes based on class probabilities
+
+        Returns:
+            tf.keras.Model: a model for performing inference
+        """
+        
+        # extract layers from trained model
+        layer_input = model_trained.get_layer('backbone-input').output
+        layer_output_mask = model_trained.get_layer('output-mask').output
+        layer_boxes = model_trained.get_layer('output-boxes').output
+        layer_labels = model_trained.get_layer('output-labels').output
+
+        # layer for decoding predictions from the object detection regression branch
         decode_boxes_centroids_offsets = ssdseglib.layers.DecodeBoxesCentroidsOffsets(
             center_x_boxes_default=self._center_x_boxes_default,
             center_y_boxes_default=self._center_y_boxes_default,
@@ -529,14 +550,32 @@ class MobileNetV2SsdSegBuilder():
             standard_deviation_center_x_offsets=self._standard_deviation_center_x_offsets,
             standard_deviation_center_y_offsets=self._standard_deviation_center_y_offsets,
             standard_deviation_width_offsets=self._standard_deviation_width_offsets,
-            standard_deviation_height_offsets=self._standard_deviation_height_offsets
-        )    
+            standard_deviation_height_offsets=self._standard_deviation_height_offsets,
+            name='decode-output-boxes'
+        )
         decode_boxes_centroids_offsets.trainable = False
 
-        non_maximum_suppression = ssdseglib.layers.NonMaximumSuppression(max_number_of_boxes_per_class=5, max_number_of_boxes_per_sample=10, boxes_iou_threshold=0.5, labels_probability_threshold=0.6)
+        # layer for processing the object detection heads outputs with non maximum suppression
+        non_maximum_suppression = ssdseglib.layers.NonMaximumSuppression(
+            max_number_of_boxes_per_class=max_number_of_boxes_per_class,
+            max_number_of_boxes_per_sample=max_number_of_boxes_per_sample,
+            boxes_iou_threshold=boxes_iou_threshold,
+            labels_probability_threshold=labels_probability_threshold,
+            name='output-object-detection'
+        )
         non_maximum_suppression.trainable = False
 
-        boxes_corners_decoded = decode_boxes_centroids_offsets(layer_output_boxes)
-        layer_output_object_detection = non_maximum_suppression(boxes_corners_coordinates=boxes_corners_decoded, labels_probabilities=layer_output_labels)
-        """
-        pass
+        # convert predicted centroids offsets to boxes corners coordinates
+        layer_boxes_decoded = decode_boxes_centroids_offsets(layer_boxes)
+
+        # keep only relevant boxes
+        layer_output_object_detection = non_maximum_suppression(boxes_corners_coordinates=layer_boxes_decoded, labels_probabilities=layer_labels)
+
+        # layers for inference model
+        model_inference = tf.keras.Model(inputs=layer_input, outputs=[layer_output_mask, layer_output_object_detection])
+
+        # transfer weights from the trained model to the inference model        
+        for layer_trained in model_trained.layers:
+            model_inference.get_layer(layer_trained.name).set_weights(layer_trained.get_weights())            
+
+        return model_inference
